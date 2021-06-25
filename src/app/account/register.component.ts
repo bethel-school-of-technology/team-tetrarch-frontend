196f5484c4bdcd79951ca13e6b1514bbb4855cba
Dane Jefferson <danejefferson1@gmail.com>
2021-06-24 22:52:49 -0700

    a few fixes

--- src/app/account/register.component.ts
+++ src/app/account/register.component.ts
@@ -21,7 +21,7 @@ export class RegisterComponent implements OnInit {
 
     ngOnInit() {
         this.form = this.formBuilder.group({
-            userName: ['', Validators.required],
+            username: ['', Validators.required],
             password: ['', [Validators.required, Validators.minLength(6)]],
             email: ['', Validators.required],
             storeName: ['', Validators.required],
