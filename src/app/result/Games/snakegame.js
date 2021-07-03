697e29dd1beeb7e13df3ea4572280440931a3f72
CodexComplex <60444313+CodexComplex@users.noreply.github.com>
2021-07-02 20:59:59 -0700

    pacman
    Signed-off-by: CodexComplex <60444313+CodexComplex@users.noreply.github.com>

--- src/app/result/Games/snakegame.js
+++ src/app/result/Games/snakegame.js
@@ -5,16 +5,3 @@ document.addEventListener("DOMContentLoaded", function () {
     pTag.appendChild(newVal);
 
   });
-  // Async Tests Example
-  /*
-  window.onModulesLoaded = new Promise( function( resolve, reject ) {
-    setTimeout(function() {
-      pTag = document.querySelector("div");
-      pTag.innerHTML = '';
-      newVal = document.createElement("p");
-      newVal.innerHTML = 'Hello World';
-      pTag.appendChild(newVal);
-      resolve();
-    }, 100)
-  });
-  */
